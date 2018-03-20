15d6e7ce6e7c218ed934fc786073fc48d73ec56c
Yanal Kashou <yanal.kashou@gmail.com>
2018-03-20 13:29:44 +0200

    Initial course creation commit DataCamp

---
+++ requirements.R
@@ -0,0 +1,12 @@
+library(remotes)
+
+##### Install specific package versions from CRAN #####
+
+# install_version("ggplot2", "2.2.1")
+# install_version("dplyr", "0.7.4")
+# install_version("tidyr", "0.8.0")
+# install_version("purrr", "0.2.4")
+
+##### Install packages in development from GitHub #####
+
+# install_github("nicholasehamilton/ggtern")
