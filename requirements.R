095814ff1e728d7d6d814a035052c4422cc75faa
Yanal Kashou <yanal.kashou@gmail.com>
2018-06-10 09:01:01 +0200

    Requirements - dplyr
    Included dplyr in required packages.

--- requirements.R
+++ requirements.R
@@ -1,15 +1,10 @@
 library(remotes)
 
 ##### Install specific package versions from CRAN #####
-
-# install_version("ggplot2", "2.2.1")
-# install_version("dplyr", "0.7.4")
-# install_version("tidyr", "0.8.0")
-# install_version("purrr", "0.2.4")
 install_version("magrittr", "1.5")
 install_version("scales", "0.5.0")
 install_version("reshape2", "1.4.3")
 install_version("lubridate", "1.7.4")
-
+install_version("dplyr", "0.7.5")
 ##### Install packages in development from GitHub #####
 install_github("tidyverse/ggplot2")
